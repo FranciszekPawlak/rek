d9110e0cc9b49638a3520a2940c849cdb1dc12a6
Franciszek Pawlak <franciszekpawlakit@gmail.com>
2019-09-26 11:09:27 +0200

    Updates

---
+++ precache-manifest.e68159b6f1a9ddbaa56746a726f76d29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e9415d3686daaecadd98b75e40ef38",
+    "url": "/FranciszekPawlak/test/index.html"
+  },
+  {
+    "revision": "e36a09f1d1dad22a31ea",
+    "url": "/FranciszekPawlak/test/static/css/main.d61b2092.chunk.css"
+  },
+  {
+    "revision": "ad7da13459c74d7a2ac9",
+    "url": "/FranciszekPawlak/test/static/js/2.ff3c7ad7.chunk.js"
+  },
+  {
+    "revision": "e36a09f1d1dad22a31ea",
+    "url": "/FranciszekPawlak/test/static/js/main.72d1095c.chunk.js"
+  },
+  {
+    "revision": "7e069bdd809a9c301c78",
+    "url": "/FranciszekPawlak/test/static/js/runtime-main.ae3aeb80.js"
+  }
+]);
