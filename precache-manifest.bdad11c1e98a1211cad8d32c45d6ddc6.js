b484eb27ed44cf44036a3044acf8b3504b83e2e0
Franciszek Pawlak <franciszekpawlakit@gmail.com>
2019-09-26 11:12:35 +0200

    Updates

---
+++ precache-manifest.bdad11c1e98a1211cad8d32c45d6ddc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5137a2ec55d05ad308085047963f6ef9",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "e36a09f1d1dad22a31ea",
+    "url": "/test/static/css/main.d61b2092.chunk.css"
+  },
+  {
+    "revision": "ad7da13459c74d7a2ac9",
+    "url": "/test/static/js/2.ff3c7ad7.chunk.js"
+  },
+  {
+    "revision": "e36a09f1d1dad22a31ea",
+    "url": "/test/static/js/main.72d1095c.chunk.js"
+  },
+  {
+    "revision": "290c1eda6ae2938a3901",
+    "url": "/test/static/js/runtime-main.16cfab28.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FranciszekPawlak/test/precache-manifest.e68159b6f1a9ddbaa56746a726f76d29.js"
+  "/test/precache-manifest.bdad11c1e98a1211cad8d32c45d6ddc6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FranciszekPawlak/test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
