70fce8b12c111d8f6ff02e3143b0b01024b90c33
Franciszek Pawlak <franciszekpawlakit@gmail.com>
2019-10-07 17:47:50 +0200

    redux hooks

--- src/index.js
+++ src/index.js
@@ -4,20 +4,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux'
-import store from './store'
-import  {ThemeProvider} from '@material-ui/styles'
-import  {createMuiTheme} from '@material-ui/core/styles'
+import {
+    ThemeProvider
+} from '@material-ui/styles'
+import {
+    createMuiTheme
+} from '@material-ui/core/styles'
 
 const theme = createMuiTheme({
-    palette:{
-        primary:{
-            main:'#67AB1C',   
+    palette: {
+        primary: {
+            main: '#67AB1C',
         },
-        secondary:{
-            main:'#fc0' 
-        }      
+        secondary: {
+            main: '#fc0'
+        }
     }
 })
 
-ReactDOM.render(<Provider store={store}><ThemeProvider theme={theme}><App /></ThemeProvider></Provider>, document.getElementById('root'));
+ReactDOM.render( < ThemeProvider theme = {
+            theme
+        } > < App / > < /ThemeProvider>, document.getElementById('root'));
